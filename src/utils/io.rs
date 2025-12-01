ee4d8fd324fffbb8b33e19aa68d05e4a73fe70ab
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-01 20:52:26 +0100

    feat: add simple IO

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,1 +1,28 @@
 // IO utility
+use std::{
+    error::Error,
+    fs::File,
+    io::{self},
+    path::Path,
+};
+
+pub fn read_file(file_path: &str) -> Result<String, io::Error> {
+    let path = Path::new(&file_path);
+
+    if !path.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "File doesn't exist",
+        ));
+    }
+
+    std::fs::read_to_string(file_path)
+}
+
+pub fn write_file(file_path: &str, contents: &str) -> Result<(), io::Error> {
+    if !Path::new(file_path).exists() {
+        let _ = File::create(file_path);
+    }
+
+    std::fs::write(file_path, contents)
+}
