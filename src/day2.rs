d7bf5e59436a4eee11c676994fe8b5d15c8ffe99
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-15 20:29:26 +0100

    feat: add new IDPair

--- src/day2.rs
+++ src/day2.rs
@@ -2,18 +2,22 @@ use std::error::Error;
 
 use crate::utils::io;
 
-
 #[derive(Debug)]
 struct IDPair {
-    bound : (u64, u64) 
+    bound: (u64, u64),
+}
+
+impl IDPair {
+    pub fn new(low: u64, high: u64) -> IDPair {
+        IDPair { bound: (low, high) }
+    }
 }
 
 pub fn day2p1() -> Result<(), Box<dyn Error>> {
     let file = io::read_file("inputs/day2")?;
-    let total : u64 = 0;
+    let total: u64 = 0;
 
     let pairs = file.split(",");
-
 
     Ok(())
 }
