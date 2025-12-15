ae27a13a0fd959a0fd968911c23a5b412dbf08fe
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-15 20:07:17 +0100

    setup for day2p1

---
+++ src/day2.rs
@@ -0,0 +1,9 @@
+use std::error::Error;
+
+use crate::utils::io;
+
+pub fn day2p1() -> Result<(), Box<dyn Error>> {
+    let file = io::read_file("inputs/day2")?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 
 mod day1;
+mod day2;
 mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
     //day1::day1p1()
-    day1::day1p2()
+    //day1::day1p2()
+    day2::day2p1()
 }
