426502f07e72d5a21bd1ce123acbe25fdad06a98
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-15 20:24:11 +0100

    split into pairs

--- src/day2.rs
+++ src/day2.rs
@@ -2,8 +2,18 @@ use std::error::Error;
 
 use crate::utils::io;
 
+
+#[derive(Debug)]
+struct IDPair {
+    bound : (u64, u64) 
+}
+
 pub fn day2p1() -> Result<(), Box<dyn Error>> {
     let file = io::read_file("inputs/day2")?;
+    let total : u64 = 0;
+
+    let pairs = file.split(",");
+
 
     Ok(())
 }
