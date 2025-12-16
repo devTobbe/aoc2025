fbe9b45ddf9a1a2e2e634b996f283a2dbc840c62
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-16 20:18:35 +0100

    day2p1 extract input

--- src/day2.rs
+++ src/day2.rs
@@ -12,7 +12,7 @@ impl IDPair {
         IDPair { range: (low, high) }
     }
 
-    fn iter(&self) -> impl Iterator<Item = u64>  {
+    fn iter(&self) -> impl Iterator<Item = u64> {
         self.range.0..self.range.1
     }
 }
@@ -20,8 +20,16 @@ impl IDPair {
 pub fn day2p1() -> Result<(), Box<dyn Error>> {
     let file = io::read_file("inputs/day2")?;
     let total: u64 = 0;
+    let mut pairs : Vec<IDPair> = Vec::new();
 
-    let pairs = file.split(",");
+    let str_pairs = file.split(",");
+
+    for pair in str_pairs {
+        let splits : Vec<&str> = pair.split("-").collect();
+        let low : u64 = splits[0].parse()?;
+        let high : u64 = splits[1].parse()?;
+        pairs.push(IDPair::new(low, high));
+    }
 
     Ok(())
 }
