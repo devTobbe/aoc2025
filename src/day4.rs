2595eb32dc0882b977bff93dbbbb69f45ac156d9
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-27 21:32:02 +0100

    d4p1 solve

--- src/day4.rs
+++ src/day4.rs
@@ -3,25 +3,53 @@ use std::error::Error;
 use crate::utils::io;
 
 pub fn day4p1() -> Result<(), Box<dyn Error>> {
-    let file = io::read_file("inputs/d4t")?;
+    let file = io::read_file("inputs/d4")?;
     let grid = parse_input(&file);
-    let total = 0;
+    let mut total = 0;
 
-    for row in grid {
-        for col in row {}
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if neighbours(&grid, col, row, 4) && grid[row][col] {
+                total += 1;
+                println!("{total}");
+            }
+        }
     }
 
     Ok(())
 }
 
-fn eval_position(grid: &[Vec<bool>], x: usize, y: usize) -> bool {
-    let slice = &grid[..=x + 1][y - 1..=y + 1];
-    for row in slice {
-        for col in row {}
-    }
-    let adjacent: usize = 0;
+// Evaluate a given position
+fn neighbours(grid: &[Vec<bool>], x: usize, y: usize, n: u64) -> bool {
+    // Locally cast for neighbour math
+    let x = x as isize;
+    let y = y as isize;
 
-    false
+    let y_roof = grid.len().saturating_sub(1) as isize;
+    let x_roof = grid[0].len().saturating_sub(1) as isize;
+
+    let floor = 0;
+    let mut adjacent: u64 = 0;
+
+    for row in y - 1..=y + 1 {
+        if is_in_bounds(row, floor, y_roof) {
+            for col in x - 1..=x + 1 {
+                if is_in_bounds(col, floor, x_roof) {
+                    if row == y && col == x {
+                        continue;
+                    }
+                    if grid[row as usize][col as usize] {
+                        adjacent += 1;
+                    }
+                }
+            }
+        }
+    }
+    adjacent < n
+}
+
+fn is_in_bounds(value: isize, floor: isize, roof: isize) -> bool {
+    floor <= value && value <= roof
 }
 
 fn parse_input(input: &str) -> Vec<Vec<bool>> {
