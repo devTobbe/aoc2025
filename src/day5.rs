714f1ebc88134b8b6af3e8685678226282da77bd
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-12-28 16:18:56 +0100

    d5p1 solve

--- src/day5.rs
+++ src/day5.rs
@@ -2,8 +2,10 @@ use std::{collections::HashSet, error::Error};
 
 use crate::utils::io;
 
+// 1040 - TOO HIGH
 pub fn day5p1() -> Result<(), Box<dyn Error>> {
-    let mut ranges: HashSet<usize> = HashSet::new();
+    // .0 lower bound .1 higher bound
+    let mut ranges: Vec<(usize, usize)> = Vec::new();
     let mut ingredients: Vec<usize> = Vec::new();
 
     let mut total = 0;
@@ -15,9 +17,12 @@ pub fn day5p1() -> Result<(), Box<dyn Error>> {
     let ranges = ranges;
     let ingredirents = ingredients;
 
-    for ingredient in ingredirents {
-        if ranges.contains(&ingredient) {
-            total += 1;
+    for ingredient in &ingredirents {
+        for range in &ranges {
+            if &range.0 <= ingredient && ingredient <= &range.1 {
+                total += 1;
+                break;
+            }
         }
     }
 
@@ -26,12 +31,11 @@ pub fn day5p1() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_input<'a>(file: &'a str, ranges: &'a mut HashSet<usize>, ingredients: &'a mut Vec<usize>) {
+fn parse_input<'a>(file: &'a str, ranges: &'a mut Vec<(usize, usize)>, ingredients: &'a mut Vec<usize>) {
     let lines: Vec<&str> = file.lines().collect();
     let mut fir_sec = true;
 
     for line in lines {
-    println!("line");
         if line.is_empty() && fir_sec {
             fir_sec = false;
             continue;
@@ -45,15 +49,13 @@ fn parse_input<'a>(file: &'a str, ranges: &'a mut HashSet<usize>, ingredients: &
     }
 }
 
-fn parse_ranges(input: &str, ranges: &mut HashSet<usize>) {
+fn parse_ranges(input: &str, ranges: &mut Vec<(usize, usize)>) {
     let bound: Vec<&str> = input.split('-').collect();
 
     let lower: usize = bound[0].parse().unwrap();
-    let high: usize = bound[1].parse().unwrap();
+    let higher: usize = bound[1].parse().unwrap();
 
-    for id in lower..=high {
-        ranges.insert(id);
-    }
+    ranges.push((lower, higher));
 }
 
 fn parse_ingredients(input: &str, ingredients: &mut Vec<usize>) {
