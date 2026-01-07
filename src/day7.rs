52a57aa92e2356f68705fad0905b2b6189c18a4c
devTobbe <32337476+devTobbe@users.noreply.github.com>
2026-01-07 21:51:08 +0100

    feat: add beam_scanner

--- src/day7.rs
+++ src/day7.rs
@@ -16,18 +16,37 @@ pub fn day7p1() -> Result<(), Box<dyn Error>> {
 }
 
 // Collects points where BEAMs should be applied
-fn bream_scanner(input: &str) -> Vec<(usize, usize)> {
-    for y in input.lines() {
-        for x in y.chars() {
-            todo!()
+fn beam_scanner(input: &str) -> Vec<(usize, usize)> {
+    let mut retvec: Vec<(usize, usize)> = vec![];
+    let lines: Vec<&str> = input.lines().collect();
+
+    // Get bounds of the input string
+    // WARNING(tobbe): Assumes that width is uniform
+    let ymax = lines.len();
+    let xmax = lines[0].len();
+    let ymin = 0;
+    let xmin = 0;
+
+    for (yi, y) in input.lines().enumerate() {
+        for (xi, x) in y.chars().enumerate() {
+            let symin = (yi - 1).clamp(ymin, ymax);
+            let symax = (yi + 1).clamp(ymin, ymax);
+
+            let sxmin = (xi - 1).clamp(xmin, xmax);
+            let sxmax = (xi + 1).clamp(xmin, xmax);
+
+            let slice = &lines[symin..symax][sxmin..sxmax];
+            if should_transform(slice) {
+                retvec.push((yi, xi));
+            }
         }
     }
-    todo!();
+    retvec
 }
 
 // Returns an updated version of the input with beams applied
-fn send_beam(input: &str) -> String {
-    let beam_cords = bream_scanner(input);
+fn draw_beam(input: &str) -> String {
+    let beam_cords = beam_scanner(input);
     todo!();
 }
 
@@ -37,6 +56,8 @@ fn should_transform(input: &[&str]) -> bool {
     // 1. Current pos is below S
     // 2. Current pos is below | ( and not ^ and not | already)
     // 3. Directly right or left of |
+    //
+    // NOTE: SHOULD skip the middle, as that's the one being evaluated
     for y in input {
         for x in y.chars() {
             todo!()
