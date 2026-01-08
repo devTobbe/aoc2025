e12cd9f4167b9ba560d4049bafb0e413cdd6d719
devTobbe <32337476+devTobbe@users.noreply.github.com>
2026-01-08 16:08:48 +0100

    day7p1 solve

--- src/day7.rs
+++ src/day7.rs
@@ -8,29 +8,10 @@ const START: char = 'S';
 const SPLITTER: char = '^';
 
 pub fn day7p1() -> Result<(), Box<dyn Error>> {
-    let file = io::read_file("inputs/d7t")?;
-    let mut beamcoors = beam_scanner(&file);
+    let file = io::read_file("inputs/d7")?;
+    let manifold = beam_scanner(&file);
 
-    let mut manifold = file.clone();
-
-    println!("{manifold}");
-    for coor in &beamcoors {
-        manifold = draw_beam(&manifold, coor);
-    }
-    println!("{manifold}");
-    let mut cont = true;
-    while cont {
-        beamcoors = beam_scanner(&manifold);
-        if beamcoors.is_empty() {
-            cont = false
-        }
-        for coor in &beamcoors {
-            manifold = draw_beam(&manifold, coor);
-        }
-        println!("{manifold}");
-    }
-
-    let total = caluclate_total(&manifold, BEAM);
+    let total = caluclate_total(&manifold);
 
     println!("{manifold}");
     println!("Total: {total}");
@@ -38,9 +19,8 @@ pub fn day7p1() -> Result<(), Box<dyn Error>> {
 }
 
 // Collects points where BEAMs should be applied
-fn beam_scanner(input: &str) -> Vec<(usize, usize)> {
-    let mut retvec: Vec<(usize, usize)> = vec![];
-    let lines: Vec<&str> = input.lines().collect();
+fn beam_scanner(input: &str) -> String {
+    let mut lines: Vec<String> = input.lines().map(String::from).collect();
 
     // Get bounds of the input string
     // WARNING(tobbe): Assumes that width is uniform
@@ -49,17 +29,17 @@ fn beam_scanner(input: &str) -> Vec<(usize, usize)> {
     let ymin: isize = 0;
     let xmin: isize = 0;
 
-    for (yi, y) in input.lines().enumerate() {
+    for yi in 0..lines.len() {
         // Skip first row
         if yi == 0 {
             continue;
         }
-        for (xi, x) in y.chars().enumerate() {
+        for xi in 0..lines[yi].len() {
             let iyi = yi as isize;
             let ixi = xi as isize;
             // WARNING: Has to be isize
             let symin = (iyi - 1).clamp(ymin, ymax - 1) as usize;
-            let symax = (iyi + 1).clamp(ymin, ymax - 1) as usize;
+            let symax = (iyi).clamp(ymin, ymax - 1) as usize;
 
             let sxmin = (ixi - 1).clamp(xmin, xmax - 1) as usize;
             let sxmax = (ixi + 1).clamp(xmin, xmax - 1) as usize;
@@ -71,33 +51,19 @@ fn beam_scanner(input: &str) -> Vec<(usize, usize)> {
                 col_slices.push(&row[sxmin..=sxmax]);
             }
 
-            if should_transform(&col_slices) {
-                retvec.push((yi, xi));
+            let mid_in_slice = (yi - symin, xi - sxmin);
+            if should_transform(&col_slices, mid_in_slice) {
+                lines[yi].replace_range(xi..=xi, &BEAM.to_string());
             }
         }
     }
-    retvec
-}
-
-// Returns an updated version of the input with beams applied
-fn draw_beam(input: &str, coor: &(usize, usize)) -> String {
-    let mut lines: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-
-    lines[coor.0][coor.1] = BEAM;
-
-    let rebuild: String = lines
-        .into_iter()
-        .map(|chars| chars.into_iter().collect::<String>())
-        .collect::<Vec<_>>()
-        .join("\n");
-
-    rebuild
+    let output: String = lines.join("\n");
+    output
 }
 
 // Determines whether or not a . should be transformed into |
-fn should_transform(input: &[&str]) -> bool {
-    let middle_y = input.len() / 2;
-    let middle_x = input[middle_y].len() / 2;
+fn should_transform(input: &[&str], middle: (usize, usize)) -> bool {
+    let (my, mx) = middle;
 
     // Conditions to draw a |
     // 1. Current pos is below S
@@ -106,7 +72,7 @@ fn should_transform(input: &[&str]) -> bool {
     //
     // NOTE: SHOULD skip the middle, as that's the one being evaluated
     // NOTE: Should probably account for 2x2 2x3 matrices
-    if let Some(middle) = input[middle_y].chars().nth(middle_x) {
+    if let Some(middle) = input[my].chars().nth(mx) {
         if middle == SPLITTER {
             return false;
         }
@@ -117,15 +83,20 @@ fn should_transform(input: &[&str]) -> bool {
     for (yi, y) in input.iter().enumerate() {
         for (xi, x) in y.chars().enumerate() {
             // Continue
-            if (yi, xi) == (middle_y, middle_x) {
+            if (yi, xi) == (my, mx) {
                 continue;
             }
 
-            // APPLY
-            if (yi, xi) == (0, 1) && (x == START || x == BEAM) {
+            // Above
+            if yi + 1 == my && xi == mx && (x == START || x == BEAM) {
                 return true;
             }
-            if (xi == 0 || xi == 2) && yi == 1 && x == SPLITTER {
+
+            // Sides
+            if yi == my && xi + 1 == mx && x == SPLITTER {
+                return true;
+            }
+            if yi == my && xi == mx + 1 && x == SPLITTER {
                 return true;
             }
         }
@@ -134,12 +105,65 @@ fn should_transform(input: &[&str]) -> bool {
 }
 
 // Takes a string and counts the amount of given item in a string
-fn caluclate_total(input: &str, item: char) -> usize {
+fn caluclate_total(input: &str) -> usize {
+    let lines: Vec<String> = input.lines().map(String::from).collect();
     let mut total = 0;
-    for char in input.chars() {
-        if char == BEAM {
-            total += 1;
+
+    // Get bounds of the input string
+    // WARNING(tobbe): Assumes that width is uniform
+    let ymax = lines.len() as isize;
+    let xmax = lines[0].len() as isize;
+    let ymin: isize = 0;
+    let xmin: isize = 0;
+
+    for yi in 0..lines.len() {
+        // Skip first row
+        if yi == 0 {
+            continue;
+        }
+        for xi in 0..lines[yi].len() {
+            let iyi = yi as isize;
+            let ixi = xi as isize;
+            // WARNING: Has to be isize
+            let symin = (iyi - 1).clamp(ymin, ymax - 1) as usize;
+            let symax = (iyi).clamp(ymin, ymax - 1) as usize;
+
+            let sxmin = (ixi - 1).clamp(xmin, xmax - 1) as usize;
+            let sxmax = (ixi + 1).clamp(xmin, xmax - 1) as usize;
+
+            let row_slice = &lines[symin..=symax];
+            let mut col_slices = Vec::new();
+
+            for row in row_slice {
+                col_slices.push(&row[sxmin..=sxmax]);
+            }
+
+            let mid_in_slice = (yi - symin, xi - sxmin);
+            if check_split(&col_slices, mid_in_slice) {
+                total += 1;
+            }
         }
     }
     total
+}
+
+fn check_split(input: &[&str], middle: (usize, usize)) -> bool {
+    let (my, mx) = middle;
+
+    if let Some(mid) = input[my].chars().nth(mx)
+        && mid == SPLITTER
+    {
+        for (yi, y) in input.iter().enumerate() {
+            for (xi, x) in y.chars().enumerate() {
+                // Continue
+                if (yi, xi) == (my, mx) {
+                    continue;
+                }
+                if yi + 1 == my && xi == mx && x == BEAM {
+                    return true;
+                }
+            }
+        }
+    }
+    false
 }
